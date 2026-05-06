abb29b910e18a72535aa673cdd9b5887a112bec0
Lucie Mingmei Hao <61232659+lucie07@users.noreply.github.com>
2026-05-07 00:44:59 +0100

    Replace NuxtImg with img
    Replaced NuxtImg component with standard img tag.

--- app/components/archive/card.vue
+++ app/components/archive/card.vue
@@ -27,7 +27,7 @@ withDefaults(defineProps<Props>(), {
 <template>
   <article class="group border dark:border-gray-800 m-2 overflow-hidden rounded-2xl shadow-sm text-zinc-700 dark:text-zinc-300">
     <NuxtLink :to="path">
-      <NuxtImg
+      <img
         class="lg:h-48 md:h-36 w-full object-cover object-center rounded-t-2xl shadow-lg group-hover:scale-[1.02] transition-all duration-500"
         width="300"
         :src="image"
