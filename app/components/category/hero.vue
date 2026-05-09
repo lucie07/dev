965606b4ee0d1f7d5138d772dd9cf56aa732d9f9
Lucie Mingmei Hao <61232659+lucie07@users.noreply.github.com>
2026-05-09 18:52:40 +0100

    Change category index intro container to full width

--- app/components/category/hero.vue
+++ app/components/category/hero.vue
@@ -3,19 +3,17 @@ import { categoryPage } from '~/data'
 </script>
 
 <template>
-  <div class="container mx-auto">
-    <div class="grid grid-cols-1 sm:grid-cols-2 items-center">
-      <div class="px-6">
-        <h1 class="text-black dark:text-zinc-300 font-semibold leading-tight text-4xl md:text-5xl my-5">
+  <section class="container mx-auto mb-6">
+    <div class="px-6 py-8 md:py-12">
+      <div class="w-full">
+        <h1 class="text-black dark:text-zinc-300 font-semibold leading-tight text-4xl md:text-5xl mb-5">
           {{ categoryPage.title }}
         </h1>
-        <p class="dark:text-zinc-300">
+
+        <p class="text-zinc-600 dark:text-zinc-300 text-base md:text-lg leading-relaxed">
           {{ categoryPage.description }}
         </p>
       </div>
-      <div class="px-6 justify-self-center">
-        <LogoDogs />
-      </div>
     </div>
-  </div>
+  </section>
 </template>
