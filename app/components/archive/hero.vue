25658e5d9a320d485d19c08893ec6da57f10b73e
Lucie Mingmei Hao <61232659+lucie07@users.noreply.github.com>
2026-05-09 18:42:13 +0100

    Change project index intro container to full width

--- app/components/archive/hero.vue
+++ app/components/archive/hero.vue
@@ -5,12 +5,12 @@ import { blogsPage } from '~/data'
 <template>
   <section class="container mx-auto mb-6">
     <div class="px-6 py-8 md:py-12">
-      <div class="max-w-4xl">
+      <div class="w-full">
         <h1 class="text-black dark:text-zinc-300 font-semibold leading-tight text-4xl md:text-5xl mb-5">
           {{ blogsPage.title }}
         </h1>
 
-        <p class="max-w-3xl text-zinc-600 dark:text-zinc-300 text-base md:text-lg leading-relaxed">
+        <p class="text-zinc-600 dark:text-zinc-300 text-base md:text-lg leading-relaxed">
           {{ blogsPage.description }}
         </p>
       </div>
