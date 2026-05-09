a1da77cc27d99748981faf83c1da11f9b5251fba
Lucie Mingmei Hao <61232659+lucie07@users.noreply.github.com>
2026-05-10 00:29:34 +0100

    Change image border radius and centre feature project image

--- app/pages/blogs/[blog].vue
+++ app/pages/blogs/[blog].vue
@@ -132,7 +132,7 @@ defineOgImageComponent('Test', {
           :tags="data.tags"
         />
         <div
-          class="prose prose-pre:max-w-xs sm:prose-pre:max-w-full prose-sm sm:prose-base md:prose-lg prose-h1:no-underline max-w-5xl mx-auto prose-zinc dark:prose-invert prose-img:rounded-lg"
+          class="prose prose-pre:max-w-xs sm:prose-pre:max-w-full prose-sm sm:prose-base md:prose-lg prose-h1:no-underline max-w-5xl mx-auto prose-zinc dark:prose-invert prose-img:rounded-none prose-img:block prose-img:mx-auto"
         >
           <ContentRenderer v-if="articles" :value="articles">
             <template #empty>
