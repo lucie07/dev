b9642aea0e958e173f1aa8e2182059bc49d499ad
Lucie Mingmei Hao <61232659+lucie07@users.noreply.github.com>
2026-05-17 23:29:58 +0100

    Change category search box colour to dark grey

--- app/components/category/topic.vue
+++ app/components/category/topic.vue
@@ -14,7 +14,7 @@ const category = computed(() => {
 
 <template>
   <div class="container mx-auto">
-    <div class="p-6 my-4 mx-2 rounded-md bg-gray-200 dark:bg-slate-900">
+    <div class="p-6 my-4 mx-2 rounded-md bg-gray-200 dark:bg-[#2E2E2E]">
       <h1 class="text-black dark:text-white font-semibold leading-tight text-xl md:text-2xl">
         #{{ category }}
       </h1>
