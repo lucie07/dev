156072f8bd9684e7c194661cd4f60c09f3a828c4
Lucie Mingmei Hao <61232659+lucie07@users.noreply.github.com>
2026-05-19 02:05:22 +0100

    Change project index card layout to 3 cols

--- app/pages/blogs/index.vue
+++ app/pages/blogs/index.vue
@@ -123,7 +123,7 @@ defineOgImage({
       />
     </div>
 
-    <div v-auto-animate class="grid grid-cols-1 md:grid-cols-2 gap-5 my-5 px-4">
+    <div v-auto-animate class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
       <template v-for="post in paginatedData" :key="post.title">
         <ArchiveCard
           :path="post.path"
