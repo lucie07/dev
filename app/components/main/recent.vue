5029f306497ba1e3d3ea66a2100a9d0eda12f66b
Lucie Mingmei Hao <luciemmh@gmail.com>
2026-05-29 22:53:27 +0100

    Increase Featured Projects font size

--- app/components/main/recent.vue
+++ app/components/main/recent.vue
@@ -58,7 +58,7 @@ useHead({
 <template>
   <div class="pb-10 px-4">
     <div class="flex flex-row items-center space-x-3 pt-5 pb-3">
-      <h2 class="text-2xl font-semibold text-black dark:text-zinc-300">Featured Projects</h2>
+      <h2 class="text-3xl font-semibold text-black dark:text-zinc-300">Featured Projects</h2>
     </div>
 
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
