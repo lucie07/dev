32ba3cdc58c5a5f0fca49595ebc9df63cf050b43
Lucie Mingmei Hao <luciemmh@gmail.com>
2026-06-25 16:07:59 +0100

    Update table of contents label

--- app/components/blog/Toc.vue
+++ app/components/blog/Toc.vue
@@ -10,7 +10,7 @@ defineProps<{
     class="fixed top-28 right-[max(2rem,calc((100vw-1024px)/2-300px))] w-56 hidden xl:block max-h-[calc(100vh-8rem)] overflow-y-auto"
   >
     <div class="border dark:border-[#5E5E5E] p-3 rounded-md dark:bg-[#2E2E2E] bg-white/90 backdrop-blur-sm shadow-lg">
-      <h2 class="text-sm font-bold mb-3 border-b dark:border-[#5E5E5E] pb-2">Table Of Content</h2>
+      <h2 class="text-sm font-bold mb-3 border-b dark:border-[#5E5E5E] pb-2">Table of Contents</h2>
       <nav>
         <NuxtLink v-for="link in links" :key="link.id" :to="`#${link.id}`" class="block text-xs mb-3 hover:underline">
           {{ link.text }}
