97ff24104833cd9bd68a3d72360d7d1cfd984320
Lucie Mingmei Hao <luciemmh@gmail.com>
2026-07-07 14:41:48 +0100

    Change web charset in Nuxt docs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,7 +36,7 @@ export default defineNuxtConfig({
   app: {
     baseURL: '/dev/',
     head: {
-      charset: 'utf-16',
+      charset: 'utf-8',
       viewport: 'width=device-width,initial-scale=1',
       title: seoData.title,
       titleTemplate: `%s - ${seoData.title}`,
