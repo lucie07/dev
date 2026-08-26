ea117e0c58222c54ab1af788e69b60cb89375136
Lucie Mingmei Hao <luciemmh@gmail.com>
2026-08-26 22:31:52 +0100

    refactor: replace 404 image with html

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -17,14 +17,18 @@ defineOgImageComponent('About', {
 </script>
 
 <template>
-  <div class="py-5">
-    <div class="container max-w-xl mx-auto">
-      <img
-        src="/404-image.png"
-        alt=""
-        width="500"
-        class="w-full h-auto"
-      >
+  <main class="py-16">
+    <div class="container max-w-xl mx-auto px-4 text-center">
+      <div class="inline-block">
+        <div class="text-8xl font-bold leading-none">
+          404
+        </div>
+        <div class="mt-2 h-2 bg-[#FFD77A]"></div>
+      </div>
+
+      <h1 class="mt-6 text-2xl font-medium">
+        Page not found
+      </h1>
     </div>
-  </div>
+  </main>
 </template>
