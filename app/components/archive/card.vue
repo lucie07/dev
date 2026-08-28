a79b3dd338fa74172ce80ed1ce9d3660062deb5b
Lucie Mingmei Hao <luciemmh@gmail.com>
2026-08-28 20:06:47 +0100

    fix: use normal <img> only for 404 fallback

--- app/components/archive/card.vue
+++ app/components/archive/card.vue
@@ -31,7 +31,15 @@ const publicPath = computed(() =>
 <template>
   <article class="group border dark:border-[#5E5E5E] m-2 overflow-hidden rounded-2xl shadow-sm text-zinc-700 dark:text-zinc-300">
     <NuxtLink :to="publicPath">
+      <img
+        v-if="image === '/404-image.png'"
+        src="/404-image.png"
+        alt=""
+        class="lg:h-48 md:h-36 w-full object-cover object-center rounded-t-2xl shadow-lg group-hover:scale-[1.02] transition-all duration-500"
+      />
+
       <NuxtImg
+        v-else
         class="lg:h-48 md:h-36 w-full object-cover object-center rounded-t-2xl shadow-lg group-hover:scale-[1.02] transition-all duration-500"
         width="300"
         :src="image"
