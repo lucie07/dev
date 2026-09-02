679a1fc04c4090611ec0ab199c2b3781b9208e48
Lucie Mingmei Hao <luciemmh@gmail.com>
2026-09-02 23:28:03 +0100

    docs(404): note unused component

--- app/components/logo/404.vue
+++ app/components/logo/404.vue
@@ -1,3 +1,4 @@
+<!-- NOTE: This component is currently not in use, see app/pages/[...slug].vue for 404 handling logic. -->
 <template>
   <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 500 500">
     <defs>
